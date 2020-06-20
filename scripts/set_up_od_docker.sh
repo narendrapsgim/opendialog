70c85a187386ac9532208e5d94681a7fc2a0fdc3
Stuart Haigh <stuarth@greenshootlabs.com>
2020-06-20 15:07:16 +0100

    reduce docker image size

--- scripts/set_up_od_docker.sh
+++ scripts/set_up_od_docker.sh
@@ -13,3 +13,7 @@ echo "Setting up the admin interface..."
 npm install -g yarn
 yarn install
 yarn run prod
+
+echo "Cleaning up install"
+rm -rf node_modules
+rm -rf vendor/opendialogai/webchat/node_modules
